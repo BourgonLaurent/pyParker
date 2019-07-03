e2abd90d07fdf24a46c777459d19204a0a5150cd
Laurent Bourgon <laurenttripleor@gmail.com>
2019-07-03 18:27:04 -0400

    Major improvements to everything. (#2)
    * Merged "wdisorting.py" in "wdisney.py", this removes some unecessary things, errors that could happen, speed up the process, remove the batch script AKA QOL changes
    * Now multiplatform by removing the need of a batch script
    * Everything is segmented in functions, maybe will be transformed in a module...
    * Now supports all 4 parks of WDW, will maybe add the other DW ...

--- wait_times.bat
+++
@@ -1,3 +0,0 @@
-"C:\Users\Laurent\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\Laurent\Python3\GitHub Projects\wdisneyw-py\wdisneyw.py"
-timeout 10
-"C:\Users\Laurent\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\Laurent\Python3\GitHub Projects\wdisneyw-py\wdisorting.py"
--- wdisneyw.py
+++ wdisneyw.py
@@ -3,92 +3,157 @@ from urllib.request import urlopen as uReq
 import urllib.request
 import datetime
 import os
+import csv
+import sys
 
-# Sources URL
-mk = "https://www.laughingplace.com/w/p/magic-kingdom-current-wait-times/" # Magic Kingdom
+def makeDir(directory):
+    if os.path.isdir(directory):
+        pass
+    else:
+        os.mkdir(directory)
+def makeFile(file):
+    if os.path.isfile(file):
+        pass
+    else:
+        open(file, 'a').close()
 
-# Accepted headers
-user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
+def verifConfig():
+    makeDir("./data/")
+    makeDir("./data/Magic Kingdom/")
+    makeDir("./data/Epcot/")
+    makeDir("./data/Disney's Hollywood Studios/")
+    makeDir("./data/Disney's Animal Kingdom/")
 
-req = urllib.request.Request(mk,headers={"User-Agent": user_agent}) # setting headers to go incognito
-uClient = uReq(req) # opening up connection
-page_html = uClient.read() # grabbing page
-uClient.close() # closing connection
+    makeFile("./data/Magic Kingdom/[INFORMATION].csv")
+    makeFile("./data/Epcot/[INFORMATION].csv")
+    makeFile("./data/Disney's Hollywood Studios/[INFORMATION].csv")
+    makeFile("./data/Disney's Animal Kingdom/[INFORMATION].csv")
 
-page_soup = soup(page_html, "lxml") #html parsing
+def retrieveHTML(selected_park):
+    global mk, ep, hs, ak
 
-title = page_soup.h1.text.strip() #Title of the page
-park = title.replace(" Current Wait Times", "") # Name of the park
-#r_time = page_soup.findAll("div", {"class":"header"})[0].findAll("span", {"id":"f_lastcheck"})[0].text.strip() # Last refresh
-
-r_datetime = page_soup.findAll("div", {"class":"header"})[0].text.strip()
-
-date, time = r_datetime.split("\n")
-
-date, ophour = date.split(": ")
-date = date.replace("Operating Hours For ", "")
-day, date = date.split(", ")
-
-time = time.replace("Last Check at ", "")
-time = time.replace(":", ".")
-
-table = page_soup.table
-containers = table.findAll("tr")
-containers = list(dict.fromkeys(containers))
+    if selected_park not in (mk, ep, hs, ak): #Check if park exists
+        print("Invalid park selected")
+        sys.exit()
+    #else: #DEBUG
+        #print(selected_park)
+    # Specify User-Agent to prevent Error 403: Forbiden
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
+    # Create url by using the park specified
+    selected_url = "https://www.laughingplace.com/w/p/{}-current-wait-times/".format(selected_park)
+    req = urllib.request.Request(selected_url,headers={"User-Agent": user_agent}) # setting headers to go incognito
+    uClient = uReq(req) # opening up connection
+    page_html = uClient.read() # grabbing page
+    uClient.close() # closing connection
+    return page_html
 
 
-t_break = "\n"
-w_break = "\n\n\n\n"
+def webScrape(page_html):
+    page_soup = soup(page_html, "lxml") #html parsing
+    title = page_soup.h1.text.strip() #Title of the page
+    park = title.replace(" Current Wait Times", "") # Name of the park
+    r_datetime = page_soup.findAll("div", {"class":"header"})[0].text.strip() #Find the locations
 
-temp = "temp.csv"
-filename = "{} - {} - {}.csv".format(date, park, time)
-f = open(temp, "w+")
+    date, time = r_datetime.split("\n")
+    date, ophour = date.split(": ")
+    date = date.replace("Operating Hours For ", "")
+    day, date = date.split(", ")
+    time = time.replace("Last Check at ", "")
+    time = time.replace(":", ".")
 
-location = "location.csv"
-z = open(location, "w+")
+    table = page_soup.table
+    containers = table.findAll("tr")
+    containers = list(dict.fromkeys(containers))
 
-loc = "{},{},{}".format(park, date, time)
-z.write(loc)
-#headers = "Attraction, Time\n"
-#f.write(headers) #CSV headers creation
+    location={}
+    location["park"]=park
+    location["ophour"]=ophour
+    location["date"]=date
+    location["day"]=day
+    location["time"]=time
 
-mklist = []
+    attlist = {}
+    for container in containers:
+        entry = container.text.strip()
+        #Cleaning entries
+        if "\n" in entry:
+            c_entry = entry.replace("\n", ",", 1)
+            c_entry = c_entry.replace('\n','')
+        if " minutes" in c_entry:
+            c_entry = c_entry.replace(" minutes", "")
+        if "“" in c_entry:
+            c_entry = c_entry.replace("“", "")
+        if "”" in c_entry:
+            c_entry = c_entry.replace("”", "")
+        if "’" in c_entry:
+            c_entry = c_entry.replace("’", "")
+        if "~" in c_entry:
+            c_entry = c_entry.replace("~", "")
+        if "Monsters, Inc. Laugh Floor" in c_entry:
+            c_entry = c_entry.replace("Monsters, Inc. Laugh Floor", "Monsters Inc. Laugh Floor")
+        if "*" in c_entry:
+            c_entry = c_entry.replace("*", "")
+        if "–" in c_entry:
+            c_entry = c_entry.replace("–", "")
+        if "!" in c_entry:
+            c_entry = c_entry.replace("!", "")
+        if ":" in c_entry:
+            c_entry = c_entry.replace(":", "")
+        if "™" in c_entry:
+            c_entry = c_entry.replace("™", "")
+        #Split entry to have 2 values
+        att, time = c_entry.split(",")
+        #Add to dictionnary
+        attlist[att] = time
+    return attlist, location
 
-for container in containers:
-    entry = container.text.strip()
-    #if "\n\n\n\n" in entry:
-    #    c_entry = entry.replace("\n\n\n\n", ", ")
-    if "\n" in entry:
-        c_entry = entry.replace("\n", ",", 1)
-        c_entry = c_entry.replace('\n','')
-    if " minutes" in c_entry:
-        c_entry = c_entry.replace(" minutes", "")
-    if "“" in c_entry:
-        c_entry = c_entry.replace("“", "")
-    if "”" in c_entry:
-        c_entry = c_entry.replace("”", "")
-    if "’" in c_entry:
-        c_entry = c_entry.replace("’", "")
-    if "~" in c_entry:
-        c_entry = c_entry.replace("~", "")
-    if "Monsters, Inc. Laugh Floor" in c_entry:
-        c_entry = c_entry.replace("Monsters, Inc. Laugh Floor", "Monsters Inc. Laugh Floor")
-    #att, tim = entry.split(", ")
-    mklist.append(c_entry)
 
-mklist = list(dict.fromkeys(mklist)) #Remove duplicates
+def writeCSV(attlist, location):
+    filename_info = "data/{}/[INFORMATION].csv".format(location["park"])
+    writeinfo = location["date"] + "," + location["day"] + "," + location["ophour"] + "\n"
 
-for i in mklist: #Write to file
-    f.write(i + "\n")
+    #Check if we need to write to [INFORMATION].csv
+    inf = open(filename_info, "r")
+    infr = inf.read()
+    headers = "Date,Day,Opening Hours\n"
+    f = open(filename_info, "a")
+    if headers not in infr:
+        f.write(headers)
+    if writeinfo not in infr:
+        f.write(writeinfo)
+    f.close()
+    inf.close()
+    for att in attlist:
+        filename = "data/{}/{}.csv".format(location["park"], att)
+        wait = str(attlist[att])
+        headers = "Date,Time,Wait\n"
+        writedata = location["date"] + "," + location["time"] + "," + wait + "\n"
+        #Write to data file if it doesn't exist
+        if os.path.isfile(filename):
+            data = open(filename, "r")
+            datar = data.read()
+            if writedata not in datar:
+                h = open(filename, "a")
+                h.write(writedata)
+                h.close()
+        else: #create file if it doesn't exist
+            newentry = open(filename, "a")
+            newentry.write(headers)
+            newentry.write(writedata)
+            newentry.close()
 
-"""
-Exemple de ce que je veux
-finalfinal = park + " - " + attraction (ex. Magic Kingdom - Astro Orbiter.csv)
-Dans le csv:
-date, time, wait
-Jul 02, 5.02pm, 30 
-"""
-#print(mklist)
+def storeWaitTimes(mainpark):
+    page_html = retrieveHTML(mainpark) #Scrape Magic Kingdom
+    attlist, location = webScrape(page_html) #Retrieve Attractions and Wait
+    writeCSV(attlist, location) #Write to a CSV
 
-#import wdisorting
-#import wdisorting
+mk = "magic-kingdom" #Source URL
+ep = "Epcot"
+hs = "Disneys-Hollywood-Studios"
+ak = "Disneys-Animal-Kingdom"
+
+verifConfig()
+storeWaitTimes(mk)
+storeWaitTimes(ep)
+storeWaitTimes(hs)
+storeWaitTimes(ak)
